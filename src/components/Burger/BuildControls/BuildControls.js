bd5230257d0a09f464d05334768352beaa9926bd
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-02-18 13:45:53 +0100

    Remove ingredient handler

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -18,7 +18,9 @@ const BuildControls = props => {
         key={ctrl.label} 
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
-        removed={() => props.ingredientRemoved(ctrl.type)} />
+        removed={() => props.ingredientRemoved(ctrl.type)} 
+        disabled={props.disabled[ctrl.type]}
+        />
       ))}
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,6 +40,9 @@ class BurgerBuilder extends Component {
 
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0){
+      return;
+    }
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
@@ -53,12 +56,19 @@ class BurgerBuilder extends Component {
 
   
   render() {
+    const disableInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disableInfo){
+      disableInfo[key] = disableInfo[key] <= 0
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler} 
-          ingredientRemoved={this.removeIngredientHandler} />
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disableInfo} />
       </Aux>
     );
   }
