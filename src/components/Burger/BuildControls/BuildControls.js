324142d5051306aaf170ede86f745359139a1357
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-02-19 15:20:31 +0100

    implement button confirm cancel order and total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,7 @@ const controls = [
 const BuildControls = props => {
   return (
     <div className={classes.BuildControls}>
-    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong> € </p>
       {controls.map(ctrl => (
         <BuildControl 
         key={ctrl.label} 
