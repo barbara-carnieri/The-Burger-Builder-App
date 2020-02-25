f785e4a355bc519607198a2b6f76c1620412a132
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-02-25 16:10:43 +0100

    add post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Beatrice Brown',
+        address: {
+          street: '23 quia road',
+          zipCode: '01234',
+          country: 'France'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethody: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+    .then( (response) => console.log(response))
+    .catch( (err) => console.log(err));
   }
   
   render() {
