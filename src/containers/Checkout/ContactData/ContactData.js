47af6ac3017b1ec4e9a5721cb8caef54420630a1
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-02-28 11:53:54 +0100

    order submit

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.module.css'
+import axios from '../../../axios-orders'
 
 class ContactData extends Component {
   state= {
@@ -9,7 +10,35 @@ class ContactData extends Component {
     address:{
       street:'',
       postalCode:''
-    }
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    // console.log(this.props.ingredients);
+    this.setState( { loading: true } );
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Max Schwarzmüller',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
   }
   
   render() {
@@ -21,7 +50,7 @@ class ContactData extends Component {
               <input className={classes.Input} type='email' name='email' placeholder='Your email'/>
               <input className={classes.Input} type='text' name='street' placeholder='Street'/>
               <input className={classes.Input} type='text' name='postal' placeholder='Postal Code'/>
-              <Button btnType='Success'>ORDER</Button>
+              <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
           </form>
       </div>
     )
