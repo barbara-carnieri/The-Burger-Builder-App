20f60c10764e7e7b95d8ed61b55ee2513187aa07
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-03-27 14:51:18 +0100

    logout implement

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = ( error ) => {
     };
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = ( email, password, isSignup ) => {
   return dispatch => {
       dispatch( authStart() );
@@ -34,14 +48,14 @@ export const auth = ( email, password, isSignup ) => {
       if(!isSignup){
           url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhFommPeskJyatlUVP6PIGyQERMUIBo-w'
       }
-      axios.post( url, authData )
-          .then( response => {
-              console.log( response );
-              dispatch( authSuccess( response.data.idToken, response.data.localId) );
-          } )
-          .catch( error => {
-            console.log( error );
-              dispatch( authFail( error ) );
-          } );
+      axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
   };
 };
