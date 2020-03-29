f3ac125ec3ada3e24525d79010f2b78a774c47eb
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-03-29 16:59:04 +0200

    remove console.log

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,7 +5,7 @@ import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = (props) => {
-    console.log(props);
+    // console.log(props);
     
     return (
         <div className={classes.CheckoutSummary}>
